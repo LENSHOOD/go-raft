59d8c1ac01d1e43480b04171e4768e619301654f
lenshood <lenshood.zxh@gmail.com>
2021-09-26 17:55:47 +0800

    feat: candidate will start election with broadcast request vote

--- core/candidate.go
+++ core/candidate.go
@@ -5,7 +5,35 @@ import "math/rand"
 type Candidate struct{ RaftBase }
 
 func (c *Candidate) TakeAction(msg Msg) Msg {
-	return Msg{}
+	switch msg.tp {
+	case Tick:
+		c.cfg.tickCnt++
+
+		if c.cfg.tickCnt == c.cfg.electionTimeout {
+			// vote myself
+			c.votedFor = c.cfg.cluster.Me
+
+			// send vote req
+			lastLogIndex := Index(-1)
+			lastLogTerm := Term(-1)
+			if lastIdx := len(c.log) - 1; lastIdx >= 0 {
+				lastLogIndex = c.log[lastIdx].Idx
+				lastLogTerm = c.log[lastIdx].Term
+			}
+			return c.broadcastReq(
+				&RequestVoteReq{
+					Term: c.currentTerm,
+					CandidateId: c.cfg.cluster.Me,
+					LastLogIndex: lastLogIndex,
+					LastLogTerm: lastLogTerm,
+				})
+		}
+
+	default:
+	}
+
+	// return null for meaningless msg
+	return NullMsg
 }
 
 func NewCandidate(f *Follower) *Candidate {
@@ -23,7 +51,7 @@ func NewCandidate(f *Follower) *Candidate {
 	electionTimeout := rand.Int63n(f.cfg.electionTimeoutMax-f.cfg.electionTimeoutMin) + f.cfg.electionTimeoutMin
 	c.cfg.electionTimeout = electionTimeout
 	// force candidate start election at first tick
-	c.cfg.tickCnt = electionTimeout
+	c.cfg.tickCnt = electionTimeout - 1
 
 	return c
 }
